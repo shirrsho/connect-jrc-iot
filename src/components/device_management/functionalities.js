539c0b22ce6a65f790dd69f8c4ad8b0262cc4d76
hasnain-shirsho-csl <bsse1106@iit.du.ac.bd>
2023-02-05 12:39:09 +0600

    add and get device ongoing

--- src/components/device_management/functionalities.js
+++ src/components/device_management/functionalities.js
@@ -1,4 +1,5 @@
 import { createNewDevice,getDevices } from "../database/regular_database_firebase";
+import Device from "./Device";
 
 // To add a new device to the database,
 // A device object should be sent to the addNew method
@@ -15,7 +16,10 @@ async function addNewDevice(device){
 async function getAllDevices(uid){
     let devices = await getDevices(uid)
     console.log(devices);
-    
+    let deviceobjs = []
+    devices.forEach(device=>{
+        // let dev = new Device(device[0],device[1].user_id,device[1].name,device[1].chip);
+    });
 }
 
 export {addNewDevice, getAllDevices}
