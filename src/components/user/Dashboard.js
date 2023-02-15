70ebaf315eeb98406ecbd16c4a6a963357252ad5
fahad <bsse1105@iit.du.ac.bd>
2023-02-15 12:57:16 +0600

    done

--- src/components/user/Dashboard.js
+++ src/components/user/Dashboard.js
@@ -12,13 +12,12 @@ import {
 } from "../device_management/functionalities";
 
 function Dashboard() {
-
-    // <-- User Management ...
-    const [user, loading, error] = useAuthState(auth);
-    function logsout() {
-      if (logout()) setUserstate(!userstate);
-    }
-    // ... User Management -->
+  // <-- User Management ...
+  const [user, loading, error] = useAuthState(auth);
+  function logsout() {
+    if (logout()) setUserstate(!userstate);
+  }
+  // ... User Management -->
 
   // <-- UI functionalities ...
   const navigate = useNavigate();
@@ -28,7 +27,7 @@ function Dashboard() {
     if (loading) {
       load();
     } else if (!user) navigate("/login");
-    else get_devices()
+    else get_devices();
   }, [user, loading, userstate]);
 
   const load = () => {
@@ -49,24 +48,23 @@ function Dashboard() {
   };
   // ... UI funcionalities -->
 
-
   // <-- Device Management ...
   const [devices, setDevices] = useState([]);
   const [device_name, setDevice_name] = useState("");
   const [chip_name, setChip_name] = useState("");
 
   async function add_device() {
-    let device = null
+    let device = null;
     try {
-      device = await addNewDevice(user.uid,device_name,chip_name);
+      device = await addNewDevice(user.uid, device_name, chip_name);
     } catch (err) {
       alert(err.message);
       return false;
     }
     setIsSaveWindowOpen(false);
-    let devscopy = devices
-    devscopy.push(device)
-    setDevices(devscopy)
+    let devscopy = devices;
+    devscopy.push(device);
+    setDevices(devscopy);
     return true;
   }
   async function get_devices() {
@@ -74,24 +72,23 @@ function Dashboard() {
   }
   // ... Device Management -->
 
-
   // <-- Search Management ...
   const [searchInput, setsearchInput] = useState("");
 
   function handleSearch() {
-    let newdevs = []
-    devices.forEach(device=>{
-      if(device.name.toLowerCase().includes(searchInput.toLowerCase())) newdevs.push(device)
+    let newdevs = [];
+    devices.forEach((device) => {
+      if (device.name.toLowerCase().includes(searchInput.toLowerCase()))
+        newdevs.push(device);
     });
-    setDevices(newdevs)
+    setDevices(newdevs);
   }
   function resetSearch() {
-    setsearchInput("")
-    get_devices()
+    setsearchInput("");
+    get_devices();
   }
   // ... Search management -->
 
-  
   return (
     <>
       <div className="flex">
@@ -109,16 +106,18 @@ function Dashboard() {
                 placeholder="Search"
                 className="px-4 py-2 border-b border-gray-400 bg-gray-200 text-black rounded-lg mt-4  w-[40%] text-2xl "
                 value={searchInput}
-                onChange={e => {setsearchInput(e.target.value);}}
+                onChange={(e) => {
+                  setsearchInput(e.target.value);
+                }}
               />
-              <button className="pl-3 text-lg" onClick={handleSearch} >
-                  {" "}
-                  Search
-                </button>
-                <button className="pl-3 text-lg" onClick={resetSearch} >
-                  {" "}
-                  Reset
-                </button>
+              <button className="pl-3 text-lg" onClick={handleSearch}>
+                {" "}
+                Search
+              </button>
+              <button className="pl-3 text-lg" onClick={resetSearch}>
+                {" "}
+                Reset
+              </button>
             </div>
             <div className=" w-[50%] relative flex-col items-center">
               <div className="flex absolute right-0 text-white text-lg bg-black p-4 rounded-sm hover:cursor-pointer hover:text-black hover:bg-white hover:text-xl">
@@ -135,33 +134,51 @@ function Dashboard() {
                   />
                 </svg>
 
-                <button className="pl-3 text-lg" onClick={handleOpen} >
+                <button className="pl-3 text-lg" onClick={handleOpen}>
                   {" "}
                   Add Device
                 </button>
                 <div>
-        
-                {isSaveWindowOpen && (
-                  <div className="fixed top-0 left-0 h-screen w-screen flex items-center  justify-center bg-gray-900 bg-opacity-75">
-                    <div className="bg-white p-8 rounded-lg">
-                      <h1 className="text-xl font-medium text-black py-5">Device Info</h1>
-                      <div className="mt-4 p-16">
-                        <input className=" mr-9 p-2 border-b placeholder-gray-600 border-gray-600 rounded-lg mt-4 text-center"
-                        type="text" placeholder="Device Name"
-                        value={device_name} onChange={e => setDevice_name(e.target.value)} />
-                        <input className=" mr-9 p-2 border-b placeholder-gray-600 border-gray-600 rounded-lg mt-4 text-center"
-                        type="text" placeholder="Chip"
-                        value={chip_name} onChange={e => setChip_name(e.target.value)} />
-                      </div>
-                      <div className="flex justify-end mt-8 py-5">
-                        <button className="bg-red-600 text-white p-3 rounded-lg mr-2" onClick={handleClose}>Cancel</button>
-                        <button className="bg-green-700 text-white p-3 rounded-lg" onClick={add_device}>Save</button>
+                  {isSaveWindowOpen && (
+                    <div className="fixed top-0 left-0 h-screen w-screen flex items-center  justify-center bg-gray-900 bg-opacity-75">
+                      <div className="bg-white p-8 rounded-lg">
+                        <h1 className="text-xl font-medium text-black py-5">
+                          Device Info
+                        </h1>
+                        <div className="mt-4 p-16">
+                          <input
+                            className=" mr-9 p-2 border-b placeholder-gray-600 border-gray-600 rounded-lg mt-4 text-center"
+                            type="text"
+                            placeholder="Device Name"
+                            value={device_name}
+                            onChange={(e) => setDevice_name(e.target.value)}
+                          />
+                          <input
+                            className=" mr-9 p-2 border-b placeholder-gray-600 border-gray-600 rounded-lg mt-4 text-center"
+                            type="text"
+                            placeholder="Chip"
+                            value={chip_name}
+                            onChange={(e) => setChip_name(e.target.value)}
+                          />
+                        </div>
+                        <div className="flex justify-end mt-8 py-5">
+                          <button
+                            className="bg-red-600 text-white p-3 rounded-lg mr-2"
+                            onClick={handleClose}
+                          >
+                            Cancel
+                          </button>
+                          <button
+                            className="bg-green-700 text-white p-3 rounded-lg"
+                            onClick={add_device}
+                          >
+                            Save
+                          </button>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )}
-              </div>
-               
+                  )}
+                </div>
               </div>
             </div>
           </div>
@@ -178,9 +195,11 @@ function Dashboard() {
                 <li className="w-[12%]">Date Added</li>
               </ul>
             </div>
-            {devices && <div>
-              <DeviceLinks devicelist={devices} />
-            </div>}
+            {devices && (
+              <div>
+                <DeviceLinks devicelist={devices} />
+              </div>
+            )}
           </div>
         </div>
       </div>
